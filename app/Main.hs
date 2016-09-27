5d8977469f9362c24c75295e599255ba4f28d723
mhweaver <mhweaver@gmail.com>
2016-09-27 11:42:33 -0600

    Removed unnecessary variable

--- app/Main.hs
+++ app/Main.hs
@@ -4,21 +4,19 @@ import Lib                ( dayNumOrDayToString
                           , parseDateStr )
 import Control.Monad      ( mplus )
 import System.Environment ( getArgs )
-import Text.Read          ( readMaybe )
-import Data.Time          ( Day )
 import System.IO          ( hPutStrLn
                           , stderr )
+import Text.Read          ( readMaybe )
+import Data.Time          ( Day )
 
 main :: IO ()
 main = do
     args <- getArgs
-    let wrappedDayNumOrDay = parseArgs args
-    case wrappedDayNumOrDay of
+    case parseArgs args of
         Left e -> hPutStrLn stderr e
         Right dayNumOrDay -> putStrLn . dayNumOrDayToString $ dayNumOrDay
 
 usage = "usage: daynum [ dddd | yyyy-mm-dd ]"
-
 parseArgs :: [String] -> Either String (Either Integer Day)
 parseArgs [arg] = case parseArg arg of
                   Nothing -> Left $ "Unable to parse date: " ++ arg ++ "\n" ++ usage
